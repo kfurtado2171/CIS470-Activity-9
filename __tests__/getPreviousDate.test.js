82e12014f910bb1d67d6b4423252d62afe09bbca
Kyle Furtado <kfurtado14@umassd.edu>
2024-04-21 23:39:15 -0400

    Done

--- __tests__/getPreviousDate.test.js
+++ __tests__/getPreviousDate.test.js
@@ -1,7 +1,8 @@
 
-const getNextDate = require('../modules/getPreviousDate');
+const getPreviousDate = require('../modules/getPreviousDate');
 
 test('Previous date test', () => {
-    // expect(getPreviousDate(4, 16, 2024)) ....
-    
+    expect(getPreviousDate(4, 1, 2024)).toEqual({ month: 3, day: 31, year: 2024 });
+    expect(getPreviousDate(2, 28, 2023)).toEqual({ month: 2, day: 27, year: 2023 });
+    expect(getPreviousDate(1, 1, 2021)).toEqual({ month: 12, day: 31, year: 2020 });
 });
--- modules/getPreviousDate.js
+++ modules/getPreviousDate.js
@@ -1,2 +1,16 @@
-// Here your code for the get previous date function goes
-// you can refer to the getNextDate function and make minor changes
+const lastDayOfMonth = require('./lastDayOfMonth');
+
+function getPreviousDate(month, day, year) {
+    const lastDay = lastDayOfMonth(month - 1, year);
+    if (day > 1) {
+        return { month, day: day - 1, year };
+    } else {
+        if (month > 1) {
+            return { month: month - 1, day: lastDay, year };
+        } else {
+            return { month: 12, day: 31, year: year - 1 };
+        }
+    }
+}
+
+module.exports = getPreviousDate;
